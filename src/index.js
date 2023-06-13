b841ddbc0bcd4b456697baff1ca1064e8578d1b4
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-06-13 22:09:18 +0300

    add imports

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+import SimpleLightbox from 'simple-lightbox.min.js';
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+// Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+// Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+// Notiflix.Notify.warning('Попередження');
+// Notiflix.Notify.info('Інфо');
+
+
