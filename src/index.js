6eee53c1e0f8a1771cb39b77f006746494053216
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-06-16 00:32:00 +0300

    js update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import "notiflix/dist/notiflix-3.2.6.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css"
 
+
+const searchForm = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+
+const axios = require('axios');
+
+
+async function fetchGetImage() {
+    const apiKey = "37262675-c60479e6538b2ce74a07e98ab";
+    try {
+        const response = await axios.get('https://pixabay.com/api/', {
+            params: {
+                apiKey
+                // ID: 12345
+
+            }
+        });
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
+
+
 // Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 // Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 // Notiflix.Notify.warning('Попередження');
