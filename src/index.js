59b2f9f6ab8f1e1eb9155efac5cb964d3c4c9274
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-06-15 22:11:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.6.min.css";
 import SimpleLightbox from "simplelightbox";
-
+import "simplelightbox/dist/simple-lightbox.min.css"
 
 // Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 // Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
